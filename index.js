e1f813ed5c074ce40bd6d1d1df7f03942fdf6181
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-12 17:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+class Cat {
+  constructor(name, gender){
+    this.name = name
+    this.gender = gender
+  }
+  speak()
+
+}
+ 
+class Dog {
+  constructor(name, gender){
+    this.name = name
+    this.gender = gender
+  }
+  speak()
+}
+ 
+class Bird {
+  constructor(name, gender){
+    this.name = name
+    this.gender = gender
+  }
+  speak()
+
+}
