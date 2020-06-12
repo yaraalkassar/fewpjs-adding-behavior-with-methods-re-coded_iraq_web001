b110ec01e343a45f6bce4f5f09058e5865b23b78
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-12 17:18:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class Bird {
   speak(){
     if(this.gender === 'male')
     {
-      return `It's me! ${this.name}`
+      return `It's me! ${this.name}, the parrot!`
     }
     else{
       return `${this.name} says squawk!`
